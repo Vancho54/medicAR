01f989f6b35d4d3f7ff02a2c12e0805126099654
Ivan <vancho54@gmail.com>
2021-02-08 12:39:37 -0300

    feat: template for medicar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main>
+        <section className='glass'>
+          <div className='dashboard'>
+            <div className='name'>
+              <h2>medic<span className='AR'>AR</span></h2>
+            </div>
+            <div className='links'>
+              <div className='link'>
+                <h3>Ingresar</h3>
+              </div>
+              <div className='link'>
+                <h3>Registrarse</h3>
+              </div>
+              <div className='link'>
+                <h3>Informacion</h3>
+              </div>
+            </div>
+            <div className='logo'>
+              <img height='60' src='https://upload.wikimedia.org/wikipedia/commons/e/ec/Ministerio_de_Salud.svg' alt=''></img>
+            </div>
+          </div>
+          <div className='form'>
+            <div className='card'>
+              <form className='form-inputs'>
+                <input placeholder='DNI' />
+                <input placeholder='Contraseña'/>
+                <select>
+                  <option>---Obra Social---</option>
+                </select>
+              </form>
+              <h2>Ingresar</h2>
+            </div>
+          </div>
+        </section>
+        <div className='circle-1'></div>
+        <div className='circle-2'></div>
+      </main>
   );
 }
 
